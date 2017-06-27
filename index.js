0f8c99260f6f81dda432bedef9b09fe49a7697b5
Richard Cornish <richardcornish@gmail.com>
2017-06-27 16:50:47 -0500

    Added noty

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var Clipboard = require('clipboard');
+var Noty = require('noty')
+
 
 var clipboard = new Clipboard('.clipboard');
+clipboard.on('success', function(e) {
+	new Noty({
+	    text: 'Copied to clipboard'
+	}).show();
+    e.clearSelection();
+});
